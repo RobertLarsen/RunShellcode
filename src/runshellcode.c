f8e1888bfc102ebee231ae7f2a82d997ad39c4f4
Robert Larsen <robert@the-playground.dk>
2020-01-22 21:54:18 +0100

    Works!

--- src/runshellcode.c
+++ src/runshellcode.c
@@ -1,23 +1,110 @@
 #include <stdio.h>
 #include <fcntl.h>
+#include <signal.h>
+#include <netinet/in.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <errno.h>
+
+char *chroot_path = NULL;
+int do_fork = 0, uid = -1, gid = -1;
+enum { Undecided, IPv4, IPv6 } ip_version = Undecided;
+void (*shellcode)() = NULL;
+
+static void child_died(int sig) {
+    wait(&sig);
+}
+
+static int create_server(unsigned short port) {
+    int server;
+    int flags;
+    int addr_size;
+    union {
+        struct sockaddr_in ipv4;
+        struct sockaddr_in6 ipv6;
+    } addr;
+
+    if (ip_version == IPv6) {
+        server = socket(AF_INET6, SOCK_STREAM, 0);
+        if (server < 0) {
+            return -1;
+        }
+
+        flags = 1;
+        if (setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &flags, sizeof(flags)) < 0) {
+            close(server);
+            return -1;
+        }
+
+        addr.ipv6.sin6_family = AF_INET6;
+        addr.ipv6.sin6_port = htons(port);
+        addr.ipv6.sin6_addr = in6addr_any;
+        addr_size = sizeof(struct sockaddr_in6);
+    } else {
+        server = socket(AF_INET, SOCK_STREAM, 0);
+        if (server < 0) {
+            return -1;
+        }
+
+        flags = 1;
+        if (setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &flags, sizeof(flags)) < 0) {
+            close(server);
+            return -1;
+        }
+
+        addr.ipv4.sin_family = AF_INET;
+        addr.ipv4.sin_port = htons(port);
+        addr.ipv4.sin_addr.s_addr = htonl(INADDR_ANY);
+        addr_size = sizeof(struct sockaddr_in);
+    }
+
+    if (bind(server, (struct sockaddr*)&addr, addr_size) < 0) {
+        close(server);
+        return -1;
+    }
+
+    if (listen(server, 10) < 0) {
+        close(server);
+        return -1;
+    }
+
+    return server;
+}
+
+void execute_shellcode() {
+    if (chroot_path && chroot(chroot_path)) {
+        exit(-1);
+    }
+
+    if (gid != -1 && setgid(gid)) {
+        exit(-1);
+    }
+
+    if (uid != -1 && setuid(uid)) {
+        exit(-1);
+    }
+
+    shellcode();
+    exit(0);
+}
 
 int main(int argc __attribute__((unused)), char ** argv __attribute__((unused))) {
     struct stat st;
-    enum { Undecided, IPv4, IPv6 } ip_version = Undecided;
-    char *chroot_path = NULL;
-    int c, opt_idx = 0, do_fork = 0, port;
+    int server, client, fd, c, opt_idx = 0, port;
+    pid_t pid;
 
     static struct option long_options[] = {
         { "ipv4",           no_argument, 0, '4' },
         { "ipv6",           no_argument, 0, '6' },
         { "fork",           no_argument, 0, 'f' },
         { "chroot",   required_argument, 0, 'c' },
-        { "userspec", required_argument, 0, 'u' },
+        { "uid",      required_argument, 0, 'u' },
+        { "gid",      required_argument, 0, 'g' },
         {          0,                 0, 0,  0  }
     };
 
@@ -47,6 +134,10 @@ int main(int argc __attribute__((unused)), char ** argv __attribute__((unused)))
                 }
                 break;
             case 'u':
+                uid = atoi(optarg);
+                break;
+            case 'g':
+                gid = atoi(optarg);
                 break;
             case '?':
                 printf("Unknown: %s\n", optarg);
@@ -54,12 +145,44 @@ int main(int argc __attribute__((unused)), char ** argv __attribute__((unused)))
         }
     }
 
+    signal(SIGCHLD, child_died);
+
     if (optind == argc) {
-        printf("Get from stdin\n");
+        /* Read shellcode from stdin */
+        shellcode = mmap(NULL, 4096, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        if (read(0, shellcode, 4096) > 0) {
+            execute_shellcode();
+        }
     } else if (stat(argv[optind], &st) == 0) {
-        printf("Get from file\n");
+        /* Read shellcode from a file */
+        if ((fd = open(argv[optind], O_RDONLY)) < 0) {
+            fprintf(stderr, "Could not open file\n");
+            exit(-1);
+        }
+        shellcode = mmap(NULL, st.st_size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE, fd, 0);
+        execute_shellcode();
     } else if ((port = atoi(argv[optind])) > 0) {
-        printf("TCP\n");
+        /* Read shellcode from TCP server */
+        if ((server = create_server(port)) < 0) {
+            fprintf(stderr, "Could not create server\n");
+            exit(-1);
+        }
+
+        do {
+            while ((client = accept(server, NULL, NULL)) >= 0) {
+                if (do_fork && (pid = fork())) {
+                    close(client);
+                } else {
+                    close(server);
+                    shellcode = mmap(NULL, 4096, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+                    if (read(client, shellcode, 4096) > 0) {
+                        execute_shellcode();
+                    }
+                    exit(0);
+                }
+            }
+        } while (errno == EINTR);
+
     } else {
         fprintf(stderr, "Cannot read shellcode from \"%s\"\n", argv[optind]);
         exit(1);
